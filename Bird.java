b2d021eb6e5be0a8d228604088cb513ef97a889e
HaredHu <hhuairar@bruinmail.slcc.edu>
2026-07-24 19:09:57 -0600

    Implement bird state management and flap functionality
    Added methods to manage bird's state and position.

--- Bird.java
+++ Bird.java
@@ -2,4 +2,37 @@ package flappyBird;
 
 public class Bird extends GameObject {
 
+  private boolean alive;
+
+  /**
+   * Creates bird.
+   */
+  public Bird(int x, int y, boolean alive){
+    super(x,y);
+    this.alive = alive;
+  }
+
+  /**
+   * Returns whether the bird is alive.
+   */
+  public boolean isAlive(){
+    return alive;
+  }
+
+  /**
+  * Bird flap upward.
+  */
+  public void flap(){
+    setY(getY()-20);
+  }
+
+  /**
+   * Reset the bird.
+   */
+  public void reset(){
+    setX();
+    setY();
+    alive = true;
+  }
+
 }

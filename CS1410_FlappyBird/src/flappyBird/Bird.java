8511ebdc5a793aadc6be5b719b1dafabbd248f03
helentle <helentle@localhost>
2026-07-24 19:13:26 -0600

    test commit

--- CS1410_FlappyBird/src/flappyBird/Bird.java
+++ CS1410_FlappyBird/src/flappyBird/Bird.java
@@ -2,5 +2,18 @@ package flappyBird;
 
 public class Bird extends GameObject {
 
+	private boolean alive;
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param alive
+	 */
+	public Bird(int x, int y, boolean alive) {
+		super(x, y);
+		this.alive = alive;
+	}
+	
+	
 
 }
--- CS1410_FlappyBird/src/flappyBird/GameObject.java
+++ CS1410_FlappyBird/src/flappyBird/GameObject.java
@@ -4,4 +4,14 @@ public class GameObject {
 
 	private int x;
 	private int y;
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public GameObject(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 }

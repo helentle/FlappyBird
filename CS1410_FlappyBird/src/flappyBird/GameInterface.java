c5424bfbb12c30089786d30922d458676ef633c7
HaredHu <hhuairar@bruinmail.slcc.edu>
2026-07-28 12:49:00 -0600

    I updated GameInterface by adding the running state(tracks if the game is active or paused),start/pause/restart method, and made the GUI visible.

--- CS1410_FlappyBird/src/flappyBird/GameInterface.java
+++ CS1410_FlappyBird/src/flappyBird/GameInterface.java
@@ -5,6 +5,7 @@ public class GameInterface {
 	private Pipe pipe;
 	private ScoreBoard scoreboard;
 	private GameGui gui;
+	private boolean running;
 		
 	/**
 	 * Creates bird, pipe, and scoreboard objects as well as the gui.
@@ -16,21 +17,45 @@ public class GameInterface {
 		
 		scoreboard = new ScoreBoard();
 		
+		running = false;
+		
 		gui = new GameGui(this);
+		gui.setVisible(true);
 		
 	}
 	
+	/**
+	 * Start the game.
+	 */
 	public void start() {
-		
+		running = true;
 	}
 	
+	/**
+	 * Pause the game.
+	 */
 	public void pause() {
-		
+		running = false;
 	}
 
+	/**
+	 * Restart all the game.
+	 */
 	public void restart() {
+		bird.reset();
+		pipe.reset();
+		scoreboard.reset();
 		
+		running =true;
 	}
+	
+	/**
+	 * Returns whether the game is running.
+	 */
+	public boolean isRunning() {
+		return running;
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
